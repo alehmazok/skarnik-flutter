f531812f66907bf99898ec3b0e8396797e1847cc
Aleh Mazok <mazokoleg@gmail.com>
2023-10-06 10:19:48 +0200

    Абнаўленне залежнасцяў праекта.

--- lib/di.skarnik.config.dart
+++ lib/di.skarnik.config.dart
@@ -13,9 +13,9 @@ import 'package:dio/dio.dart' as _i11;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:skarnik_flutter/features/app/data/repository/dev_analytics_app_repository.dart'
-    as _i4;
-import 'package:skarnik_flutter/features/app/data/repository/firebase_analytics_app_repository.dart'
     as _i5;
+import 'package:skarnik_flutter/features/app/data/repository/firebase_analytics_app_repository.dart'
+    as _i4;
 import 'package:skarnik_flutter/features/app/data/repository/objectbox_database_repository.dart'
     as _i10;
 import 'package:skarnik_flutter/features/app/data/service/objectbox_service.dart'
@@ -35,15 +35,15 @@ import 'package:skarnik_flutter/features/app/domain/use_case/init_remote_config.
 import 'package:skarnik_flutter/features/app/domain/use_case/log_analytics_app_started.dart'
     as _i26;
 import 'package:skarnik_flutter/features/history/data/repository/objectbox_history_repository.dart'
-    as _i16;
+    as _i19;
 import 'package:skarnik_flutter/features/history/domain/repository/history_repository.dart'
-    as _i15;
+    as _i18;
 import 'package:skarnik_flutter/features/history/domain/use_case/load_history.dart'
     as _i24;
 import 'package:skarnik_flutter/features/home/data/repository/objectbox_history_repository.dart'
-    as _i19;
+    as _i21;
 import 'package:skarnik_flutter/features/home/domain/repository/history_repository.dart'
-    as _i18;
+    as _i20;
 import 'package:skarnik_flutter/features/home/domain/use_case/load_history.dart'
     as _i25;
 import 'package:skarnik_flutter/features/search/data/repository/objectbox_search_repository.dart'
@@ -65,7 +65,7 @@ import 'package:skarnik_flutter/features/translation/data/repository/dev_analyti
 import 'package:skarnik_flutter/features/translation/data/repository/firebase_analytics_translation_repository.dart'
     as _i7;
 import 'package:skarnik_flutter/features/translation/data/repository/objectbox_history_repository.dart'
-    as _i21;
+    as _i16;
 import 'package:skarnik_flutter/features/translation/data/repository/objectbox_word_repository.dart'
     as _i40;
 import 'package:skarnik_flutter/features/translation/data/repository/skarnik_translation_repository.dart'
@@ -73,7 +73,7 @@ import 'package:skarnik_flutter/features/translation/data/repository/skarnik_tra
 import 'package:skarnik_flutter/features/translation/domain/repository/analytics_translation_repository.dart'
     as _i6;
 import 'package:skarnik_flutter/features/translation/domain/repository/history_repository.dart'
-    as _i20;
+    as _i15;
 import 'package:skarnik_flutter/features/translation/domain/repository/translation_repository.dart'
     as _i35;
 import 'package:skarnik_flutter/features/translation/domain/repository/word_repository.dart'
@@ -95,8 +95,8 @@ import 'package:skarnik_flutter/features/vocabulary/domain/repository/vocabulary
 import 'package:skarnik_flutter/features/vocabulary/domain/use_case/load_vocabulary.dart'
     as _i44;
 
-const String _dev = 'dev';
 const String _prod = 'prod';
+const String _dev = 'dev';
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -110,12 +110,12 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     gh.factory<_i3.AnalyticsAppRepository>(
-      () => _i4.DevAnalyticsAppRepository(),
-      registerFor: {_dev},
+      () => _i4.FirebaseAnalyticsAppRepository(),
+      registerFor: {_prod},
     );
     gh.factory<_i3.AnalyticsAppRepository>(
-      () => _i5.FirebaseAnalyticsAppRepository(),
-      registerFor: {_prod},
+      () => _i5.DevAnalyticsAppRepository(),
+      registerFor: {_dev},
     );
     gh.factory<_i6.AnalyticsTranslationRepository>(
       () => _i7.FirebaseAnalyticsTranslationRepository(),
@@ -142,9 +142,9 @@ extension GetItInjectableX on _i1.GetIt {
     gh.factory<_i23.InitRemoteConfigUseCase>(
         () => _i23.InitRemoteConfigUseCase());
     gh.factory<_i24.LoadHistoryUseCase>(
-        () => _i24.LoadHistoryUseCase(gh<_i15.HistoryRepository>()));
+        () => _i24.LoadHistoryUseCase(gh<_i18.HistoryRepository>()));
     gh.factory<_i25.LoadHistoryUseCase>(
-        () => _i25.LoadHistoryUseCase(gh<_i18.HistoryRepository>()));
+        () => _i25.LoadHistoryUseCase(gh<_i20.HistoryRepository>()));
     gh.factory<_i26.LogAnalyticsAppOpenUseCase>(() =>
         _i26.LogAnalyticsAppOpenUseCase(gh<_i3.AnalyticsAppRepository>()));
     gh.factory<_i27.LogAnalyticsShareUseCase>(() =>
@@ -154,7 +154,7 @@ extension GetItInjectableX on _i1.GetIt {
         _i28.LogAnalyticsTranslationUseCase(
             gh<_i6.AnalyticsTranslationRepository>()));
     gh.factory<_i29.SaveToHistoryUseCase>(
-        () => _i29.SaveToHistoryUseCase(gh<_i20.HistoryRepository>()));
+        () => _i29.SaveToHistoryUseCase(gh<_i15.HistoryRepository>()));
     gh.factory<_i30.SearchRepository>(
         () => _i31.ObjectboxSearchRepository(gh<_i17.ObjectboxService>()));
     gh.factory<_i32.SearchUseCase>(
