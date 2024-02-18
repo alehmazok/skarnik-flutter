02d2fcfe95396e552fd8e6469e6bf5d9ea48b978
Aleh Mazok <mazokoleg@gmail.com>
2024-02-18 13:05:28 +0100

    test: coverage of vocabulary_cubit_test.dart

---
+++ test/features/vocabulary/vocabulary_cubit_test.dart
@@ -0,0 +1,131 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:skarnik_flutter/features/app/domain/entity/word.dart';
+import 'package:skarnik_flutter/features/vocabulary/domain/repository/vocabulary_repository.dart';
+import 'package:skarnik_flutter/features/vocabulary/domain/use_case/load_vocabulary.dart';
+import 'package:skarnik_flutter/features/vocabulary/presentation/vocabulary_cubit.dart';
+
+class MockVocabularyRepository extends Mock implements VocabularyRepository {}
+
+class MockTickerProvider extends Mock implements TickerProvider {}
+
+class MockTicker extends Mock implements Ticker {
+  @override
+  String toString({bool debugIncludeStack = false}) => 'Mock Ticker';
+}
+
+class MockWord extends Mock implements Word {}
+
+void main() {
+  group('VocabularyCubit', () {
+    final tickerProvider = MockTickerProvider();
+    setUp(() {
+      when(
+        () => tickerProvider.createTicker(any()),
+      ).thenReturn(
+        MockTicker(),
+      );
+    });
+
+    VocabularyCubit newInstance(VocabularyRepository vocabularyRepository) => VocabularyCubit(
+          loadVocabularyUseCase: LoadVocabularyUseCase(vocabularyRepository),
+          tickerProvider: tickerProvider,
+        );
+
+    group('loadWords()', () {
+      final vocabularyRepository = MockVocabularyRepository();
+      late final Word word1;
+      late final Word word2;
+
+      blocTest(
+        'emits failed state when failed to get words from database',
+        setUp: () {
+          when(
+            () => vocabularyRepository.getWords(any()),
+          ).thenThrow(
+            UnimplementedError('test get words error'),
+          );
+        },
+        build: () => newInstance(vocabularyRepository),
+        act: (cubit) => cubit.loadWords(1),
+        expect: () => [
+          isA<VocabularyInitedState>(),
+          isA<VocabularyFailedState>().having(
+            (state) => (state.error as UnimplementedError).message,
+            'message',
+            equals('test get words error'),
+          ),
+        ],
+      );
+
+      blocTest(
+        'emits ok state when successfully got words from database',
+        setUp: () {
+          word1 = MockWord();
+          word2 = MockWord();
+
+          when(
+            () => vocabularyRepository.getWords(any()),
+          ).thenAnswer(
+            (_) async => [word1, word2],
+          );
+        },
+        build: () => newInstance(vocabularyRepository),
+        act: (cubit) => cubit.loadWords(1),
+        expect: () => [
+          isA<VocabularyInitedState>(),
+          isA<VocabularyLoadedState>().having(
+            (state) => state.words,
+            'words',
+            equals([word1, word2]),
+          ),
+        ],
+      );
+    });
+
+    group('loadWords() from cache', () {
+      final vocabularyRepository = MockVocabularyRepository();
+      late final Word word1;
+      late final Word word2;
+
+      blocTest(
+        'emits ok state when successfully got words from cache',
+        setUp: () {
+          word1 = MockWord();
+          word2 = MockWord();
+
+          when(
+            () => vocabularyRepository.getWords(any()),
+          ).thenAnswer(
+            (_) async => [word1, word2],
+          );
+        },
+        build: () => newInstance(vocabularyRepository),
+        act: (cubit) async {
+          cubit.loadWords(1);
+          await Future.delayed(const Duration(milliseconds: 100));
+          cubit.loadWords(1);
+        },
+        expect: () => [
+          isA<VocabularyInitedState>(),
+          isA<VocabularyLoadedState>().having(
+            (state) => state.words,
+            'words',
+            equals([word1, word2]),
+          ),
+          isA<VocabularyInitedState>(),
+          isA<VocabularyLoadedState>().having(
+            (state) => state.words,
+            'words',
+            equals([word1, word2]),
+          ),
+        ],
+        verify: (_) {
+          verify(() => vocabularyRepository.getWords(1)).called(1);
+        },
+      );
+    });
+  });
+}
