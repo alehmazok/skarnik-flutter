994954c1f2c3b06d7fe571e704c55ea58a8dea8f
Aleh Mazok <mazokoleg@gmail.com>
2024-04-23 23:23:18 +0200

    merge with master

--- lib/features/vocabulary/domain/repository/vocabulary_repository.dart
+++ lib/features/vocabulary/domain/repository/vocabulary_repository.dart
@@ -2,6 +2,4 @@ import 'package:skarnik_flutter/features/app/domain/entity/word.dart';
 
 abstract class VocabularyRepository {
   Future<Iterable<Word>> getWords(int langId);
-
-  Stream<Iterable<Word>> getStream(int langId);
 }
