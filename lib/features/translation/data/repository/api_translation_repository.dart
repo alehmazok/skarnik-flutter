5e60b72067240319799d0ba63250918f2d6060cd
Aleh Mazok <mazokoleg@gmail.com>
2024-08-14 14:06:57 +0200

    chore: Лакальнае лагіраванне запытаў да АПІ.

--- lib/features/translation/data/repository/api_translation_repository.dart
+++ lib/features/translation/data/repository/api_translation_repository.dart
@@ -4,6 +4,7 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:injectable/injectable.dart';
 import 'package:skarnik_flutter/app_config.dart';
 import 'package:skarnik_flutter/features/app/domain/entity/word.dart';
+import 'package:skarnik_flutter/logging.dart';
 
 import '../../domain/entity/api_word.dart';
 import '../../domain/entity/translation.dart';
@@ -11,6 +12,8 @@ import '../../domain/repository/translation_repository.dart';
 
 @Injectable(as: PrimaryTranslationRepository)
 class ApiTranslationRepository implements PrimaryTranslationRepository {
+  final _logger = getLogger(ApiTranslationRepository);
+
   final Dio _dio;
 
   ApiTranslationRepository(this._dio);
@@ -21,6 +24,7 @@ class ApiTranslationRepository implements PrimaryTranslationRepository {
       AppConfig.apiHostName,
       'api/words/${word.dictionary.path}/${word.wordId}/',
     );
+    _logger.fine('Робім запыт на: ${uri.toString()}');
 
     final res = await _dio.getUri(
       uri,
