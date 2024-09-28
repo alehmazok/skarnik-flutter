2e6e1c3a3cb6d74b27e82472c39d3fbf63b66ed7
Aleh Mazok <mazokoleg@gmail.com>
2024-09-28 23:21:39 +0200

    refactor: Адрас распрацоўшчыкаў вынесены ў AppConfig.

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -9,6 +9,8 @@ abstract class AppConfig {
 
   static const apiHostName = String.fromEnvironment('API_HOSTNAME');
 
+  static const devEmailAddress = String.fromEnvironment('DEV_EMAIL_ADDRESS');
+
   static const typesenseHostName = String.fromEnvironment('TYPESENSE_HOSTNAME');
 
   static const typesenseApiKey = String.fromEnvironment('TYPESENSE_API_KEY');
